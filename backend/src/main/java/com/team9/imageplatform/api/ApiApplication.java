41e9b0cce0522539111d31f67c8221579800ac1e
Hwang Soojin <soojin9433@naver.com>
2025-11-20 20:52:17 +0900

    chore(api): S3 연결 테스트용 PostConstruct 메서드 제거
    - 초기 Object Storage 연동 확인용으로 사용하던 testS3 메서드 삭제
    - 실제 서비스 코드에서 불필요한 콘솔 로그 및 의존성 정리

--- backend/src/main/java/com/team9/imageplatform/api/ApiApplication.java
+++ backend/src/main/java/com/team9/imageplatform/api/ApiApplication.java
@@ -1,23 +1,12 @@
 package com.team9.imageplatform.api;
 
-import com.amazonaws.services.s3.AmazonS3;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-	@Autowired
-	private AmazonS3 s3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@PostConstruct
-	public void testS3() {
-		System.out.println("=== S3 버킷 목록 테스트 ===");
-		s3.listBuckets().forEach(b -> System.out.println(b.getName()));
 	}
 }
